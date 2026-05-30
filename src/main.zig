487ff85455bdbadc1c25a208deff6aac969e721c
Brohudev <hitarth.thanki9006@gmail.com>
2026-05-30 17:46:03 -0500

    bruh. i forgot i have two copies of spacedout. this is cursed

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,12 @@
-const std = @import("std");
-const Io = std.Io;
+//this is the canonical entry point. I am assuming this is responsible for:
+// 1. init all the resources
+// 2. start the main loop.
+// 3. destroy all memory and resources at close, and handle all the high level errors and exceptions
 
+const std = @import("std");
 const spacedout = @import("spacedout");
 
 pub fn main() !void {
-    // Prints to stderr, unbuffered, ignoring potential errors.
-    std.debug.print("hello from inside the main function of the app!\n", .{});
+    std.debug.print("Hello, {s}!\n", .{"World"});
 }
 
